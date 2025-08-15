d716be5157036102eb11ac65a4ab831cbb8c6749
Bubbu Dert <139119578+BubbuDert249@users.noreply.github.com>
2025-08-15 16:45:41 +0200

    Update pydirectx.h

--- pydirectx.h
+++ pydirectx.h
@@ -1,56 +1,67 @@
 #pragma once
-
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <stdexcept>
 #include <string>
+#include <stdexcept>
+#include <vector>
+#include <memory>
 
-namespace pydirectx {
+namespace py = pybind11;
 
-enum class Version {
-    DX10 = 10,
-    DX11 = 11,
-    DX12 = 12
-};
+// Supported versions
+enum class DXVersion { DX10 = 10, DX11 = 11, DX12 = 12 };
 
-enum class APIType {
-    DX,
-    D3D,
-    BOTH
-};
-
-class DirectX {
-public:
-    DirectX(Version version);
-    void render_text(int x, int y, const std::string &text, int width = 100, int height = 20);
-
-private:
-    Version version_;
-    void check_support();
-};
-
-class Direct3D {
-public:
-    Direct3D(Version version);
-    void render_triangle(int x, int y, int z, int width, int height, int rotation, int depth, const std::string &texture = "");
-    void render_box(int x, int y, int z, int width, int height, int rotation, int depth, const std::string &texture = "");
-    void render_circle(int x, int y, int z, int width, int height, int rotation, int depth, const std::string &texture = "");
-    void render_3dtext(int x, int y, int z, int width, int height, int rotation, int depth, const std::string &text, const std::string &texture = "");
-    void remove_triangle();
-    void remove_box();
-    void remove_circle();
-    void remove_3dtext();
-
-private:
-    Version version_;
-    void check_support();
-};
+// Forward declarations for D3D/DX classes
+class D3DRenderer;
+class DXRenderer;
 
 class PyDirectX {
 public:
-    static void init(int version, const std::string &api);
-    static DirectX dx;
-    static Direct3D d3d;
+    PyDirectX() : version(DXVersion::DX12) {}
+
+    void init(int ver);  // Initialize DirectX/D3D
+
+    std::unique_ptr<D3DRenderer> d3d;
+    std::unique_ptr<DXRenderer> dx;
+
+private:
+    DXVersion version;
 };
 
-} // namespace pydirectx
+// ----------- D3D Commands -----------
+class D3DRenderer {
+public:
+    void render_triangle(float x, float y, float z,
+                         float height, float width,
+                         float rotation, float depth,
+                         const std::string& texture = "");
+    void render_box(float x, float y, float z,
+                    float height, float width,
+                    float rotation, float depth,
+                    const std::string& texture = "");
+    void render_circle(float x, float y, float z,
+                       float height, float width,
+                       float rotation, float depth,
+                       const std::string& texture = "");
+    void render_3dtext(float x, float y, float z,
+                       float height, float width,
+                       float rotation, float depth,
+                       const std::string& text,
+                       const std::string& texture = "");
+
+    void remove_triangle(int id);
+    void remove_box(int id);
+    void remove_circle(int id);
+    void remove_3dtext(int id);
+
+private:
+    std::vector<int> objects; // simple object registry
+};
+
+// ----------- DX Commands -----------
+class DXRenderer {
+public:
+    void render_text(const std::string& text, float x, float y, float size);
+    void render_box(float x, float y, float width, float height, const std::string& color = "white");
+    void clear();
+};
